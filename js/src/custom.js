1409d572cc88d1eb91a4b05682a0c9a76807d87f
iwits <hvergelmir07@gmail.com>
2019-06-30 00:39:12 +0800

    Site updated: 2019-06-30 00:39:03

---
+++ js/src/custom.js
@@ -0,0 +1,11 @@
+/*控制台*/
+if(window.console) {
+	var cons = console; 
+	if(cons) { 
+		cons.group("O(∩_∩)O哈喽！");
+		cons.info("这位看代码的童鞋，不如留下你的友链来一起玩耍吧！"); 
+		cons.info("如果有发现什么bug，还请帮忙留言提醒，或者到我的GitHub项目里提交issue哦~ %c(https://github.com/lewky/lewky.github.io)","color:#a517ef;font-weight:bold;");
+		cons.log("%cYulin Lewis's Blog", "background-image:-webkit-gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );color:transparent;-webkit-background-clip: text;font-size:5em;");
+		cons.groupEnd();
+	} 
+}
